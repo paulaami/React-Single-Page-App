7aecc980b7785271c3f6d75db9fc50001b6c8875
paulaami <pmilkowska@onet.pl>
2022-02-24 15:33:42 +0100

    added collection-item page

--- src/pages/CollectionItem.js
+++ src/pages/CollectionItem.js
@@ -1,7 +1,13 @@
 import Navbar from "../components/Navigation/Navbar";
+import Header from "../components/Header/Header";
 
 const SinglePage = () => {
- return <Navbar></Navbar>
+ return (
+ <div className="subpage collection__page collection__item">
+  <Navbar></Navbar>
+  <Header title="Collection"></Header>
+  </div>
+  )
 }
 
 export default SinglePage;
